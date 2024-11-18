f1baa9a3eeac21d993235a4c48aeeae5c0a891f5
Nikita <35460261+SnowPowerCore@users.noreply.github.com>
2024-11-18 12:41:01 +0300

    - Fix;

--- Shared/Services/IAM/Extensions/ApplicationUserExtensions.cs
+++ Shared/Services/IAM/Extensions/ApplicationUserExtensions.cs
@@ -8,9 +8,8 @@ public static class ApplicationUserExtensions
     public static ApplicationUser ToEntity(this CreateUser createUser) =>
         new()
         {
-            UserName = !string.IsNullOrEmpty(createUser.LastName)
-                ? $"{createUser.FirstName} {createUser.LastName}"
-                : $"{createUser.FirstName}",
+            FirstName = createUser.FirstName,
+            LastName = !string.IsNullOrEmpty(createUser.LastName) ? createUser.LastName : string.Empty,
             Email = createUser.Email,
             PhoneNumber = createUser.PhoneNumber,
             AccessFailedCount = 0,
