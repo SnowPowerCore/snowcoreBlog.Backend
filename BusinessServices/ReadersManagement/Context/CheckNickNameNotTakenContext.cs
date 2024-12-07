1eaa5f1969cfd637baa287fa77d35a0048970b5e
Nikita <35460261+SnowPowerCore@users.noreply.github.com>
2024-12-08 01:19:30 +0300

    - Explicit check nickname endpoint;

---
+++ BusinessServices/ReadersManagement/Context/CheckNickNameNotTakenContext.cs
@@ -0,0 +1,8 @@
+﻿using MinimalStepifiedSystem.Base;
+
+namespace snowcoreBlog.Backend.ReadersManagement.Context;
+
+public class CheckNickNameNotTakenContext(string nickName) : BaseGenericContext
+{
+    public string NickName { get; } = nickName;
+}
---
+++ BusinessServices/ReadersManagement/Delegates/CheckNickNameNotTakenDelegate.cs
@@ -0,0 +1,7 @@
+﻿using Results;
+using snowcoreBlog.Backend.ReadersManagement.Context;
+using snowcoreBlog.PublicApi.BusinessObjects.Dto;
+
+namespace snowcoreBlog.Backend.ReadersManagement.Delegates;
+
+public delegate Task<IResult<NickNameNotTakenCheckResultDto>> CheckNickNameNotTakenDelegate(CheckNickNameNotTakenContext context, CancellationToken token = default);
