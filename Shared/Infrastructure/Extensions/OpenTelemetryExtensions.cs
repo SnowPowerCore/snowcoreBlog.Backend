cad2d4076c3de310541ac55688e592bc3de1d9c8
Nikita <35460261+SnowPowerCore@users.noreply.github.com>
2024-12-09 17:20:55 +0300

    - Full https 🔥

---
+++ Shared/Infrastructure/Extensions/OpenTelemetryExtensions.cs
@@ -0,0 +1,28 @@
+﻿using FastEndpoints.OpenTelemetry;
+using MassTransit.Logging;
+using MassTransit.Monitoring;
+using Npgsql;
+using OpenTelemetry;
+using OpenTelemetry.Trace;
+
+namespace snowcoreBlog.Backend.Infrastructure.Extensions;
+
+public static class OpenTelemetryExtensions
+{
+    public static OpenTelemetryBuilder ConnectBackendServices(this OpenTelemetryBuilder builder) =>
+        builder
+            .WithTracing(static tracing =>
+            {
+                tracing.AddFastEndpointsInstrumentation();
+                tracing.AddRedisInstrumentation();
+                tracing.AddNpgsql();
+                tracing.AddSource(DiagnosticHeaders.DefaultListenerName);
+                tracing.AddSource("Marten");
+            })
+            .WithMetrics(static metrics =>
+            {
+                metrics.AddNpgsqlInstrumentation();
+                metrics.AddMeter("Marten");
+                metrics.AddMeter(InstrumentationOptions.MeterName);
+            });
+}
