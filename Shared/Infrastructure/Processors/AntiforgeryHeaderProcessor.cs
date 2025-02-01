e735a8adac1ec1aee9c2e0ea4682ecc4f20c935f
Nikita <35460261+SnowPowerCore@users.noreply.github.com>
2025-02-01 20:21:52 +0300

    - tmp;

---
+++ Shared/Infrastructure/Processors/AntiforgeryHeaderProcessor.cs
@@ -0,0 +1,29 @@
+using NJsonSchema;
+using NSwag;
+using NSwag.Generation.Processors;
+using NSwag.Generation.Processors.Contexts;
+using snowcoreBlog.Backend.Core.Constants;
+using snowcoreBlog.PublicApi.Constants;
+
+namespace snowcoreBlog.Backend.Infrastructure.Processors;
+
+public class AntiforgeryHeaderProcessor : IOperationProcessor
+{
+    public bool Process(OperationProcessorContext context)
+    {
+        var operation = context.OperationDescription.Operation;
+        if (operation.Tags.Any(x => x == EndpointTagConstants.RequireCaptchaVerification))
+        {
+            operation.Parameters.Add(new OpenApiParameter()
+            {
+                Name = HeaderKeyConstants.CaptchaHeader,
+                Kind = OpenApiParameterKind.Header,
+                Schema = new JsonSchema { Type = JsonObjectType.String },
+                IsRequired = true,
+                Description = "A required base64 captcha solution that has to be sent along the request."
+            });
+        }
+
+        return true;
+    }
+}
