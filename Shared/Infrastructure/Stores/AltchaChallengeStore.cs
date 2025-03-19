712f4eefab7cd997287e24585a223a543c6a19fe
Nikita <35460261+SnowPowerCore@users.noreply.github.com>
2025-03-19 12:02:18 +0300

    - Altcha stable;

--- Shared/Infrastructure/Stores/AltchaChallengeStore.cs
+++ Shared/Infrastructure/Stores/AltchaChallengeStore.cs
@@ -5,7 +5,7 @@ using snowcoreBlog.Backend.Infrastructure.Entities;
 
 namespace snowcoreBlog.Backend.Infrastructure.Stores;
 
-public class AltchaChallengeStore : IAltchaChallengeStore
+public class AltchaChallengeStore : IAltchaCancellableChallengeStore
 {
     private readonly string _connectionString;
 
@@ -14,7 +14,7 @@ public class AltchaChallengeStore : IAltchaChallengeStore
         _connectionString = configuration.GetConnectionString("db-snowcore-blog-entities")!;
     }
 
-    public async Task<bool> Exists(string challenge)
+    public async Task<bool> Exists(string challenge, CancellationToken cancellationToken)
     {
         using var store = DocumentStore.For(opts =>
         {
@@ -23,23 +23,23 @@ public class AltchaChallengeStore : IAltchaChallengeStore
         await using var session = store.LightweightSession();
         session.DeleteWhere<AltchaStoredChallengeEntity>(storedChallenge =>
             storedChallenge.ExpiryUtc <= DateTimeOffset.UtcNow);
+        await session.SaveChangesAsync(cancellationToken);
         return session.Query<AltchaStoredChallengeEntity>().Any(storedChallenge =>
             storedChallenge.Challenge == challenge);
     }
 
-    public async Task Store(string challenge, DateTimeOffset expiryUtc)
+    public async Task Store(string challenge, DateTimeOffset expiryUtc, CancellationToken cancellationToken)
     {
         using var store = DocumentStore.For(opts =>
         {
             opts.Connection(_connectionString);
         });
         await using var session = store.LightweightSession();
-        using var ct = new CancellationTokenSource();
         session.Store(new AltchaStoredChallengeEntity()
         {
             Challenge = challenge,
             ExpiryUtc = expiryUtc
         });
-        await session.SaveChangesAsync(ct.Token);
+        await session.SaveChangesAsync(cancellationToken);
     }
 }
