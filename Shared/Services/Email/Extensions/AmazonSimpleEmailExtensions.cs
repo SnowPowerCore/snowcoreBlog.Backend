cb04b0cdf8c0fc4707eca6eae92e11e590a309df
Nikita <35460261+SnowPowerCore@users.noreply.github.com>
2025-05-21 02:46:26 +0300

    - Amazon SES2 extensions impl;

--- Shared/Services/Email/Extensions/AmazonSimpleEmailExtensions.cs
+++ Shared/Services/Email/Extensions/AmazonSimpleEmailExtensions.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using Amazon.SimpleEmailV2.Model;
 using Riok.Mapperly.Abstractions;
 using snowcoreBlog.Backend.Email.Core.Contracts;
@@ -8,26 +9,27 @@ namespace snowcoreBlog.Backend.Email.Extensions;
 public static partial class AmazonSimpleEmailExtensions
 {
     [MapProperty(nameof(SendGenericEmail.SenderAddress), nameof(SendEmailRequest.FromEmailAddress))]
-    [MapProperty(nameof(SendGenericEmail.SenderAddress), nameof(SendEmailRequest.Destination))]
-    public static partial SendEmailRequest ToAmazonSimpleEmailRequest(this SendGenericEmail genericEmail);
-    // new()
-    // {
-    //     Source = model.SenderAddress,
-    //     Destination = new Destination
-    //     {
-    //         ToAddresses = new List<string> { model.NotifiedEntityAddress }
-    //     },
-    //     Message = new Message
-    //     {
-    //         Subject = new Content(model.Subject),
-    //         Body = new Body
-    //         {
-    //             Html = new Content
-    //             {
-    //                 Charset = "UTF-8",
-    //                 Data = model.Content
-    //             }
-    //         }
-    //     }
-    // };
+    [MapProperty(nameof(SendGenericEmail.Subject), nameof(@SendEmailRequest.Content.Simple.Subject.Data))]
+    [MapProperty(nameof(SendGenericEmail.Content), nameof(@SendEmailRequest.Content.Simple.Body.Text.Data))]
+    [MapProperty(nameof(SendGenericEmail.Content), nameof(@SendEmailRequest.Content.Simple.Body.Html.Data))]
+    [MapValue(nameof(@SendEmailRequest.Content.Simple.Body.Text.Charset), Use = nameof(GetUtf8EncodingStr))]
+    [MapValue(nameof(@SendEmailRequest.Content.Simple.Body.Html.Charset), Use = nameof(GetUtf8EncodingStr))]
+    private static partial SendEmailRequest MapperToAmazonSimpleEmailRequest(this SendGenericEmail genericEmail);
+
+    private static string GetUtf8EncodingStr() => Encoding.UTF8.WebName.ToUpper();
+
+    public static SendEmailRequest ToAmazonSimpleEmailRequest(this SendGenericEmail genericEmail)
+    {
+        var request = MapperToAmazonSimpleEmailRequest(genericEmail);
+        request.Destination ??= new();
+        if (request.Destination.ToAddresses is not default(List<string>))
+        {
+            request.Destination.ToAddresses.Add(genericEmail.ReceiverAddress);
+        }
+        else
+        {
+            request.Destination.ToAddresses = [genericEmail.ReceiverAddress];
+        }
+        return request;
+    }
 }
